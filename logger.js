dfc8b48bf2711c233861c93338593c562fc0930d
W.A.Punsisi Yemani Perera <wapunsisiyemani18@gmail.com>
2025-04-16 18:05:59 +0530

    updated

--- logger.js
+++ logger.js
@@ -2,12 +2,12 @@ import winston from "winston";
 
 // Set up Winston logger
 const logger = winston.createLogger({
-    level: "info",
-    format: winston.format.simple(),
-    transports: [
-      new winston.transports.File({ filename: "logs/error.log", level: "error" }),
-      new winston.transports.File({ filename: "logs/combined.log" }),
-    ],
-  });
+  level: "info", // Set the log level you want
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/combined.log" }), // Ensure this is correct
+  ],
+});
 
 export default logger;
