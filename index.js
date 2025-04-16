402acfe153db9e5df5365a9d385009611016958a
W.A.Punsisi Yemani Perera <wapunsisiyemani18@gmail.com>
2025-04-16 18:00:36 +0530

    Update the index.js and logger.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import morgan from "morgan";
-import winston from "winston";
+import logger from './logger.js'; // imported logger
 
-import logger from './logger.js';
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
 import orderRouter from "./routers/orderRouter.js";
@@ -15,11 +14,11 @@ const app = express();
 
 //mongodb+srv://admin:123@cluster0.ldsnt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 mongoose.connect("mongodb+srv://admin:123@cluster0.ldsnt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
-    ()=>{
+    ()=> {
         console.log("Connected to the database");
     }
 ).catch(
-    ()=>{
+    ()=> {
         console.log("Connection failed!");
     }
 )
@@ -27,44 +26,30 @@ mongoose.connect("mongodb+srv://admin:123@cluster0.ldsnt.mongodb.net/?retryWrite
 // Middleware: Logging
 app.use(morgan("dev")); // logs HTTP requests to console
 
-// Set up Winston logger
-const logger = winston.createLogger({
-    level: "info",
-    format: winston.format.simple(),
-    transports: [
-      new winston.transports.File({ filename: "logs/error.log", level: "error" }),
-      new winston.transports.File({ filename: "logs/combined.log" }),
-    ],
-  });
-  
-  // Example custom log
-  logger.info("App started");
-  
-  // Optional: Log errors
-  app.use((err, req, res, next) => {
+// Example custom log (this should now use the imported logger)
+logger.info("App started");
+
+// Optional: Log errors
+app.use((err, req, res, next) => {
     logger.error(err.stack);
     res.status(500).send("Something broke!");
-  });
+});
 
-//middleware
+// Middleware
 app.use(bodyParser.json());
-//create a middleware
 app.use(verifyJWT);
 
-//routers connection-----------------------
-//connect the studentrouter---if a http request comes as /student
-//please connect it to the studentRouter
-app.use("/api/user",userRouter);
-app.use("/api/product",productRouter);
-app.use("/api/order",orderRouter);
+// Routers connection
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/order", orderRouter);
 
-//A small route to check if the app is alive...
+// A small route to check if the app is alive...
 app.get('/health', (req, res) => {
     res.status(200).json({ status: "ok" });
-  });
+});
 
-//add a comment
-app.listen(4000,()=>{
+// Start server
+app.listen(4000, () => {
     logger.info("Server is running on port 4000");
-})
-  
+});
