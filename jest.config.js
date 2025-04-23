4d4579cb49dbdfc9db3bdd9d82a7d4decf796f0a
Yemani Perera <147752581+WAPunsisiYemaniPerera@users.noreply.github.com>
2025-04-24 01:00:34 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,1 @@
-module.exports = {
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["lcov", "text-summary"],
-  collectCoverageFrom: [
-    "src/**/*.{js,jsx,ts,tsx}",
-    "!**/node_modules/**",
-    "!**/test/**",
-    "!**/mock/**"
-  ]
-};
+
